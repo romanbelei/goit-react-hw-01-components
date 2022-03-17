8ba57f89e5617db1bc1a0b8ebc18073ac11d69f0
romanbelei <45937404+romanbelei@users.noreply.github.com>
2022-03-18 00:27:27 +0200

    Ok

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,24 +2,24 @@ import PropTypes from "prop-types";
 import s from "./FriendListItem.module.css"
 
 const FriendListItem = ({
-        key,
         avatar,
         name,
         isOnline,
 }) => {
     return (
-        <li key={key} class={s.item}>
+        <li  className={s.item}>
             <span className={isOnline ? s['online'] :  s['Offline']}></span>
             <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={s.name}>{name}</p>
         </li>
-    ); 
+    );
+    
 };
 
 FriendListItem.propTypes = {
     vatar: PropTypes.string,
     name: PropTypes.string,
-    isOnline: PropTypes.string,
+    isOnline: PropTypes.bool,
 }
 
 export default FriendListItem;
